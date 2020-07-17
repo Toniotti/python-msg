73e74ecd9068731571735f542179c5a89ed28caf
Cloud User <ec2-user@ip-172-31-23-100.sa-east-1.compute.internal>
2020-07-17 21:27:32 +0000

    att

--- msg.py
+++ msg.py
@@ -10,39 +10,57 @@ sock.bind(('0.0.0.0', 8889))
 sock.listen(6)
 
 class Connection:
-    def __init__(self, con, con_hash, pkey, qtd_msg):
-        self.con  = con
+    def __init__(self, con, con_hash, pkey, userName):
+        self.con      = con
         self.con_hash = con_hash
-        sekf.pkey = pkey
-        self.qtd_msg = qtd_msg
+        self.pkey     = pkey
+        self.userName = userName
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4)
+
+class RecMessage:
+    def __init__(self, isReg, con_hash, msg):
+        self.msg      = msg
+        self.con_hash = con_hash
+        self.isReg    = isReg
+        
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                sort_keys=True, indent=4)
+
+class RegMessage:
+    def __init__(self, userName, pkey):
+        self.userName = userName
+        self.pkey     = pkey
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                sort_keys=True, indent=4)
 
 
 connections = []
 
 def handler(c, a):
     while True:
-        data = c.recv(2048)
-        smsg = Object()
-        smsg.message = str(data.decode('utf-8'))
-        smsg.address = a
-        for idc, con in enumerate(connections):
-            if con.con != c:
-                con.send(bytes(smsg.toJSON(), 'UTF-8'))
-            else:
-                if con.qtd_msg < 2:
-                    connections[idc].pkey = smsg.message
-                
-                
-        if not data:
-            connections.remove(c)
-            c.close()
-            break
+        data    = c.recv(2048)
+        rec_msg = json.load(StringIO(str(data.decode('utf-8'))))
 
-system('cls')
+        if rec_msg.isReg == True:
+            reg_msg = json.load(StringIO(rec_msg.msg))
+            for icd, reg_con in enumerate(connections):
+                if reg_con.con_hash == rec_msg.con_hash:
+                    reg_con.userName = reg_msg.userName
+                    reg_con.pkey     = reg_msg.pkey
+                else:
+                    reg_con.con.send(bytes(RecMessage(True, reg_con.con_hash, reg_msg.pkey)), 'UTF-8')
+        else:
+            for idc, con in enumerate(connections):
+                if con.con != c:
+                    con.con.send(bytes(RecMessage(False, rec_msg.con_hash, rec_msg.msg)), 'UTF-8')
+
+system('clear')
 print('#Server started...')
 while True:
     c, a = sock.accept()
