15ee5c6f3d82f92f8467029fafaa8b6b963de866
toniotti <matheus.toniotti@gmail.com>
2020-07-17 18:30:18 -0300

    att user

--- msg-user.py
+++ msg-user.py
@@ -10,24 +10,63 @@ system('cls')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print('#Connection information.')
-ip = str(input('#Address: '))
-port = int(input('#Port: '))
+ip       = str(input('#Address: '))
+port     = int(input('#Port: '))
+user_name =  str(input('#UserName: '))
 system('cls')
 
 print('#Connecting...')
-s.connect((ip, port))
+try:
+    s.connect((ip, port))
+except:
+    system.exit()
 print('#Connected.')
-print(str(s.getsockname()).encode())
-print(hashlib.md5(str(s.getsockname()).encode()).hexdigest())
+user_hash = hashlib.md5(str(s.getsockname()).encode()).hexdigest()
 
+class Connection:
+    def __init__(self, con_hash, pkey, userName):
+        self.con_hash = con_hash
+        self.pkey     = pkey
+        self.userName = userName
 
+class RecMessage:
+    def __init__(self, isReg, con_hash, msg):
+        self.msg      = msg
+        self.con_hash = con_hash
+        self.isReg    = isReg
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                sort_keys=True, indent=4)
+
+class RegMessage:
+    def __init__(self, userName, pkey):
+        self.userName = userName
+        self.pkey     = pkey
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                sort_keys=True, indent=4)
+
+users_con = []
 
 def msg_handler():
     while True:
-        data = json.load(StringIO(s.recv(2048).decode('utf-8')))
+        data = s.recv(2048)
+        rec_msg = json.load(StringIO(data.decode('utf-8')))
         if not data:
             break
-        print('<%s>: %s' % (data['address'], data['message']))
+        
+        if rec_msg.isReg == True:
+            reg_msg = json.load(StringIO(rec_msg.msg))
+            users_con.append(Connection(rec_msg.con_hash, reg_msg.pkey, reg_msg.userName))
+            print(reg_msg.pkey)
+        else:
+            for user in users_con:
+                print('<%s>: %s' % (user.userName, rec_msg.msg))
+reg = str(RecMessage(True, user_hash, RegMessage(user_name, 'kkk')).toJSON())
+tst = bytes(reg, 'UTF-8')
+s.send(tst)
 
 rt = threading.Thread(target=msg_handler)
 rt.daemon = True
